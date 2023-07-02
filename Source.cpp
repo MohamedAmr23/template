7dbcb6ef6f6384edeeec5f9aeaf267fa41d21577
MohamedAmr23 <102430066+MohamedAmr23@users.noreply.github.com>
2023-07-02 15:35:48 +0300

    Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
-template<class t,class t2>
+template<class t,class t2>      //template in function
 t2 add(t num1, t2 num2) {
 	return num1 + num2;
 }
-template<class t3,class t4>
+template<class t3,class t4>       //template in class
 class number {
 	t3 n;
 public:
@@ -17,8 +17,8 @@ public:
 };
 
 int main() {
-	//cout << add<int,double>(10, 20.2) << endl;  //template in function
-	number<int, double> num(10);      //template in class
-	cout << num.add1(10, 10.2) << endl;
+	cout << add<int,double>(10, 20.2) << endl;  		//template in function
+	number<int, double> num(10);     			 //template in class
+	cout << num.add1(10, 10.2) << endl;   			//template in class
 	return 0;
 }
